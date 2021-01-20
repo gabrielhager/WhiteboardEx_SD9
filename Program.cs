d67a432003d27d28b2a9a365e6853506f6dfb1e4
Gabriel Hager <gabriel.a.hager@gmail.com>
2021-01-20 09:40:04 -0800

    Initial Upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhiteboardEx_SD9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whiteboard Solution Container");
+        }
+    }
+}
